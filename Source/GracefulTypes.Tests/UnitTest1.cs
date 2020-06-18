f2724b0553afe6452c3730f08f955a99738f3ff9
José Manuel Nieto Sánchez <superjmn@outlook.com>
2020-06-18 23:31:27 +0200

    Code moved to "Source" folder

---
+++ Source/GracefulTypes.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace GracefulTypes.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            // TODO: Port tests from Grace or make new tests!
+        }
+    }
+}
